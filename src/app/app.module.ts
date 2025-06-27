c8830c190f588f4193ba8e9af8c9e4aff42a0389
martinswilliam <williamgmca@gmail.com>
2025-06-27 16:39:21 -0300

    feat(ui): Build login and register forms with Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Importações do Angular Material
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card'; // Vamos usar um card para ficar bonito
 
 @NgModule({
   declarations: [
@@ -14,7 +21,12 @@ import { RegisterComponent } from './pages/register/register.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]

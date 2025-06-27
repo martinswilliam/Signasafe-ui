21728d90a8c5d94b3f06a80741fa99bd907dd733
martinswilliam <williamgmca@gmail.com>
2025-06-27 16:25:11 -0300

    feat(ui): Create login and register components and configure routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component'; // Importa o componente de login
+import { RegisterComponent } from './pages/register/register.component'; // Importa o componente de registro
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Adiciona as novas rotas aqui
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
